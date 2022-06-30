f4fa6c080e9b749a10192d86b82f11b19393d5c6
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-30 23:25:41 +0200

    fix: changing cors

--- votie-backend/app.js
+++ votie-backend/app.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(cookieParser());
+
+app.use(cors({
+    origin: 'http://localhost:19006'
+}));
+
 
 app.use(morgan("dev"));
 app.use(express.json({ limit: "150mb" }));
 
-// const whiteList = ["http://localhost:3000"];
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: function (origin, callback) {
-//       if (whiteList.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("not allowed by CORS"));
-//       }
-//     },
-//   })
-// );
 
 // define the routes here
 app.use("/users", require("./routes/userRoutes"));
