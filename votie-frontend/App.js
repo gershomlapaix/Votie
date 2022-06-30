3e005eecc76195a1abaad7ba4c2f50d682a6932a
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-30 22:17:14 +0200

    feat: bring down to the web

--- votie-frontend/App.js
+++ votie-frontend/App.js
@@ -1,60 +1,10 @@
-// import * as React from 'react';
-// import { Button, View } from 'react-native';
-// import VotingScreen from './components/voting';
-// // import HomeScreen from './components/Home';
-// import ResultsScreen from './components/Results';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { NavigationContainer } from '@react-navigation/native';
-
-// function HomeScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button
-//         onPress={() => navigation.navigate('voting page')}
-//         title="Go to notifications"
-//       />
-//     </View>
-//   );
-// }
-
-// function NotificationsScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button onPress={() => navigation.goBack()} title="Go back home" />
-//     </View>
-//   );
-// }
-
-// const Drawer = createDrawerNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
-//         <Drawer.Screen name="Home" component={HomeScreen} />
-//         <Drawer.Screen name="voting page" component={VotingScreen} />
-//         <Drawer.Screen name="Results" component={ResultsScreen} />
-//       </Drawer.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-
 import * as React from 'react';
 import { Button, View } from 'react-native';
+import VotingScreen from './components/voting';
+import HomeScreen from './components/Home';
+import ResultsScreen from './components/Results';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to "
-      />
-    </View>
-  );
-}
 
 function NotificationsScreen({ navigation }) {
   return (
@@ -71,7 +21,8 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="voting page" component={VotingScreen} />
+        <Drawer.Screen name="Results" component={ResultsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- votie-frontend/components/voting.js
+++ votie-frontend/components/voting.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Pressable, Image } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, Pressable, Image } from "react-native";
 
-export default function Voting({navigation}) {
+export default function Voting({ navigation }) {
   return (
     <View style={styles.votingContainer}>
       <View style={styles.cards}>
@@ -10,7 +10,7 @@ export default function Voting({navigation}) {
             <Image
               style={styles.cardImage}
               source={{
-                uri: 'https://m0.her.ie/wp-content/uploads/2018/01/07093633/GettyImages-887815620.jpg',
+                uri: "https://decider.com/wp-content/uploads/2022/02/The-Worst-Person-in-the-World.jpg?quality=75&strip=all&w=1200",
               }}
             />
 
@@ -19,17 +19,20 @@ export default function Voting({navigation}) {
               <Text>Technical director</Text>
             </View>
 
-            <Pressable style={styles.voteBtn} onPress={()=>{
-              navigation.navigate("Results")
-            }}>
-              <Text style={{ color: '#fff' }}>Vote</Text>
+            <Pressable
+              style={styles.voteBtn}
+              onPress={() => {
+                navigation.navigate("Results");
+              }}
+            >
+              <Text style={{ color: "#fff" }}>Vote</Text>
             </Pressable>
           </View>
           <View style={styles.card}>
             <Image
               style={styles.cardImage}
               source={{
-                uri: 'https://thumbs.dreamstime.com/b/happy-person-portrait-smiling-woman-tanned-skin-curly-hair-happy-person-portrait-smiling-young-friendly-woman-197501184.jpg',
+                uri: "https://thumbs.dreamstime.com/b/happy-person-portrait-smiling-woman-tanned-skin-curly-hair-happy-person-portrait-smiling-young-friendly-woman-197501184.jpg",
               }}
             />
 
@@ -39,14 +42,14 @@ export default function Voting({navigation}) {
             </View>
 
             <Pressable style={styles.voteBtn}>
-              <Text style={{ color: '#fff' }}>Vote</Text>
+              <Text style={{ color: "#fff" }}>Vote</Text>
             </Pressable>
           </View>
           <View style={styles.card}>
             <Image
               style={styles.cardImage}
               source={{
-                uri: 'https://decider.com/wp-content/uploads/2022/02/The-Worst-Person-in-the-World.jpg?quality=75&strip=all&w=1200',
+                uri: "https://decider.com/wp-content/uploads/2022/02/The-Worst-Person-in-the-World.jpg?quality=75&strip=all&w=1200",
               }}
             />
 
@@ -56,14 +59,14 @@ export default function Voting({navigation}) {
             </View>
 
             <Pressable style={styles.voteBtn}>
-              <Text style={{ color: '#fff' }}>Vote</Text>
+              <Text style={{ color: "#fff" }}>Vote</Text>
             </Pressable>
           </View>
           <View style={styles.card}>
             <Image
               style={styles.cardImage}
               source={{
-                uri: 'https://m0.her.ie/wp-content/uploads/2018/01/07093633/GettyImages-887815620.jpg',
+                uri: "https://www.mnp.ca/-/media/foundation/integrations/personnel/2020/12/16/13/57/personnel-image-4483.jpg?h=800&w=600&hash=9D5E5FCBEE00EB562DCD8AC8FDA8433D",
               }}
             />
 
@@ -73,12 +76,14 @@ export default function Voting({navigation}) {
             </View>
 
             <Pressable style={styles.voteBtn}>
-              <Text style={{ color: '#fff' }}>Vote</Text>
+              <Text style={{ color: "#fff" }}>Vote</Text>
             </Pressable>
           </View>
         </View>
 
-        <Text style={{fontWeight:'800', fontSize:16}}>You will view the results after voting</Text>
+        <Text style={{ fontWeight: "800", fontSize: 16 }}>
+          You will view the results after voting
+        </Text>
       </View>
     </View>
   );
@@ -86,36 +91,41 @@ export default function Voting({navigation}) {
 
 const styles = StyleSheet.create({
   votingContainer: {
-    width: '100%',
+    width: "100%",
   },
   cards: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
 
   card: {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'row',
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
     marginVertical: 10,
-    borderWidth: '2',
-    borderColor: 'grey',
+    borderWidth: 0.5,
+    borderColor: "#d7d9db",
     padding: 20,
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity:0.1,
+    shadowRadius:5
   },
   voteBtn: {
-    backgroundColor: 'blue',
-    fontWeight: '800',
+    backgroundColor: "blue",
+    fontWeight: "800",
     paddingVertical: 10,
     paddingHorizontal: 25,
+    borderRadius: 5,
   },
   cardImage: {
     height: 60,
     width: 60,
-    borderRadius: '50%',
+    borderRadius: "50%",
   },
   description: {
     marginHorizontal: 20,
-    fontWeight: '800',
+    fontWeight: "800",
   },
 });
