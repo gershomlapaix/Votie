c6fc8b408dd7c608096ec897372e08f358e73821
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-30 20:11:09 +0200

    feat: home navigation

--- votie-frontend/App.js
+++ votie-frontend/App.js
@@ -1,21 +1,42 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Pressable, Button } from 'react-native';
 import Constants from 'expo-constants';
-import Voting from './components/voting'
+import VotingScreen from './components/voting';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const Home = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home screen</Text>
+
+      <Button
+        title="Go to home"
+        onPress={() => {
+          navigation.navigate('voting page');
+        }}
+      />
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Voting/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="voting page" component={VotingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:40,
+    marginTop: 40,
     paddingTop: Constants.statusBarHeight,
     padding: 8,
   },
- 
 });
