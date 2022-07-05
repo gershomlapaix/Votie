526464079cdc08efbc9623f455f97d167f71266b
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-05 21:34:23 +0200

    feat: remove swagger

--- votie-backend/routes/userRoutes.js
+++ votie-backend/routes/userRoutes.js
@@ -9,35 +9,6 @@ router
 
   // define the swgger document for this API
 
-  /**
-   * @swagger
-   * /users:
-   *  get:
-   *    summary: Get all from the database
-   *    description: Get all users in the application
-   *    responses:
-   *      200:
-   *        description: A list of users
-   *        content:
-   *         application/json:
-   *          schema:
-   *            type: array
-   *            properties:
-   *              data:
-   *                type: array
-   *                items:
-   *                  type: object
-   *                 properties:
-   *                  id:
-   *                    type: integer
-   *                    description: The user ID.
-   *                    example: 0
-   *                  name:
-   *                    type: string
-   *                    description: The user's name.
-   *                    example: Leanne Graham
-   *
-   * */
   .get(userController.getAllUsers);
 
 router.post("/login", userController.signin);
